a4cfd4533dc26c7d08c380689a8a8176bba30d77
morteza matbou <mortezamatbou91@gmail.com>
2017-08-26 17:31:29 +0430

    index file (site entry point)
    This is a test

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title>GitHub Test giTest</title>
+</head>
+
+<body>
+
+	<h1>GitHub Test</h1>
+	
+	<p>
+		This is a test
+	</p>
+
+</body>
+
+</html>
---
+++ panel/dashboard/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title>Dashboard (giTest)</title>
+</head>
+
+<body>
+
+	<h1>Dashboard</h1>
+	
+	<p>
+		This is a test
+	</p>
+
+</body>
+
+</html>
